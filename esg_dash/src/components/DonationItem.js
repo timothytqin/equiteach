5a6124ac6292b65735e461deb828de7819bb18e5
Steve Han <stevehan2001@gmail.com>
2021-10-02 13:53:13 -0500

    style changes

--- esg_dash/src/components/DonationItem.js
+++ esg_dash/src/components/DonationItem.js
@@ -4,67 +4,71 @@ import { formatDonationAmount } from "../lib/lib";
 import { TYPOGRAPHY, PRIMARY_COLOR } from "../styles/css/global";
 
 export default function DonationItem(props) {
-	const { rank, ticker, data } = props;
-	const styles = {
-		background: {
-			backgroundColor: rank % 2 == 1 ? "#F3F3F3" : "#FFF",
-			width: window.innerWidth * 0.2,
-			padding: 10,
-			borderBottom: "1px solid #D5D5D5",
-		},
-		rankContainer: {
-			flex: 1,
-			justifyContent: "center",
-			alignItems: "center",
-		},
-		tickerContainer: {
-			flex: 3,
-			alignItems: "center",
-		},
-		amountContainer: {
-			flex: 2,
-			justifyContent: "center",
-			alignItems: "center",
-		},
-		rank: { fontSize: 32 },
-		ticker: { fontSize: 28 },
-		amount: { fontSize: 24 },
-	};
+  const { rank, ticker, data } = props;
+  const styles = {
+    background: {
+      // backgroundColor: rank % 2 == 1 ? "#F3F3F3" : "#FFF",
+      backgroundColor: "#FFF",
+      //   width: window.innerWidth * 0.1,
+      padding: 10,
+      //   borderBottom: "1px solid #D5D5D5",
+    },
+    rankContainer: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    tickerContainer: {
+      flex: 3,
+      alignItems: "center",
+    },
+    amountContainer: {
+      flex: 2,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: PRIMARY_COLOR,
+      borderRadius: 10,
+      color: "white",
+    },
+    rank: { fontSize: 22, fontWeight: 500, color: "#888" },
+    ticker: { fontSize: 18 },
+    amount: { fontSize: 18 },
+  };
 
-	return (
-		<Grid container item style={styles.background}>
-			<Grid container style={styles.rankContainer}>
-				<Typography
-					style={{
-						...TYPOGRAPHY,
-						...styles.rank,
-					}}
-				>
-					{rank}
-				</Typography>
-			</Grid>
-			<Grid container style={styles.tickerContainer}>
-				<Typography
-					style={{
-						...TYPOGRAPHY,
-						...styles.ticker,
-					}}
-				>
-					{ticker}
-				</Typography>
-			</Grid>
-			<Grid container style={styles.amountContainer}>
-				{data[ticker] && data[ticker].donation && (
-					<Typography
-						style={{
-							...TYPOGRAPHY,
-							...styles.amount,
-						}}
-					>
-						${formatDonationAmount(`${data[ticker].donation}`)}
-					</Typography>
-				)}
-			</Grid>
-		</Grid>
-	);
+  return (
+    <Grid container item style={styles.background}>
+      <Grid container style={styles.rankContainer}>
+        <Typography
+          style={{
+            ...TYPOGRAPHY,
+            ...styles.rank,
+          }}
+        >
+          {rank}
+        </Typography>
+      </Grid>
+      <Grid container style={styles.tickerContainer}>
+        <Typography
+          style={{
+            ...TYPOGRAPHY,
+            ...styles.ticker,
+          }}
+        >
+          {ticker}
+        </Typography>
+      </Grid>
+      <Grid container style={styles.amountContainer}>
+        {data[ticker] && data[ticker].donation && (
+          <Typography
+            style={{
+              ...TYPOGRAPHY,
+              ...styles.amount,
+            }}
+          >
+            ${formatDonationAmount(`${data[ticker].donation}`)}
+          </Typography>
+        )}
+      </Grid>
+    </Grid>
+  );
 }
