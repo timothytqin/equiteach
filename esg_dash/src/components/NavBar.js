3bdc44cbb43e1cb0c5f03e0cf2a5bd9602ed9dac
Steve Han <stevehan2001@gmail.com>
2021-10-02 10:24:07 -0500

    made a button. Ikr huge progress

--- esg_dash/src/components/NavBar.js
+++ esg_dash/src/components/NavBar.js
@@ -3,36 +3,49 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import { PRIMARY_COLOR, TYPOGRAPHY } from "../styles/css/global";
 import logo from "../styles/assets/logo_light.png";
-import { Typography } from "@material-ui/core";
+import { Typography, Button } from "@material-ui/core";
 
 export default function NavBar(props) {
-	return (
-		<div>
-			<AppBar
-				position="static"
-				elevation={0}
-				style={{
-					backgroundColor: `${PRIMARY_COLOR}80`,
+  return (
+    <div>
+      <AppBar
+        position="static"
+        elevation={0}
+        style={{
+          backgroundColor: `${PRIMARY_COLOR}`,
 
-					height: window.innerHeight * 0.09,
-					justifyContent: "center",
-				}}
-			>
-				<Toolbar>
-					<img
-						style={{
-							width: 60,
-							height: "80%",
-							marginRight: 10,
-						}}
-						src={logo}
-						alt="Equiteach"
-					/>
-					<Typography style={{ ...TYPOGRAPHY, fontSize: 36 }}>
-						Equiteach
-					</Typography>
-				</Toolbar>
-			</AppBar>
-		</div>
-	);
+          height: window.innerHeight * 0.09,
+          justifyContent: "center",
+        }}
+      >
+        <Toolbar>
+          <img
+            style={{
+              width: 60,
+              height: "80%",
+              marginRight: 10,
+            }}
+            src={logo}
+            alt="Equiteach"
+          />
+          <Typography style={{ ...TYPOGRAPHY, fontSize: 36 }}>
+            Equiteach
+          </Typography>
+          <Button
+            style={{
+              backgroundColor: "#2f2e40",
+              color: "white",
+              fontWeight: 800,
+              fontSize: "1rem",
+              textTransform: "capitalize",
+              marginLeft: "auto",
+              padding: ".5rem 2rem",
+            }}
+          >
+            Make a donation
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
