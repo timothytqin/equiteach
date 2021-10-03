a01193950f6a56ec3e26c39c815bd6511116f89c
Steve Han <stevehan2001@gmail.com>
2021-10-03 05:53:42 -0500

    done with quiz screen

--- equiteach_app/navigation/HomeStack.js
+++ equiteach_app/navigation/HomeStack.js
@@ -2,22 +2,23 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "../screens/Home";
 import SessionDetails from "../screens/SessionDetails";
+import Quiz from "../screens/Quiz";
 
 const Stack = createStackNavigator();
 
 export default function ProfileNavigator({ navigation }) {
-	return (
-		<Stack.Navigator>
-			<Stack.Screen
-				name="Home"
-				component={Home}
-				options={{ headerShown: false }}
-			/>
-			<Stack.Screen
-				name="SessionDetails"
-				component={SessionDetails}
-				options={{ headerShown: false }}
-			/>
-		</Stack.Navigator>
-	);
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="SessionDetails"
+        component={SessionDetails}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
 }
---
+++ equiteach_app/screens/Quiz.js
@@ -0,0 +1,119 @@
+import React, { useEffect, useState } from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { useFonts } from "expo-font";
+import Container from "../components/Container";
+import { useNavigation } from "@react-navigation/native";
+import tutor from "../assets/tutor.png";
+import pfp from "../assets/pfp.png";
+import CustomButton from "../components/CustomButton";
+import { theme } from "../theme";
+import CustomText from "../components/CustomText";
+import Card from "../components/Card";
+import { Icon } from "react-native-elements";
+import { useSelector } from "react-redux";
+import api from "../api/backendApi";
+import FlipCard from "react-native-flip-card";
+
+export default function Quiz() {
+  const [quiz, setQuiz] = useState([]);
+  const [loaded] = useFonts({
+    A: require("../assets/A.ttf"),
+    F: require("../assets/F.ttf"),
+  });
+
+  useEffect(() => {
+    fetch("http://8668-12-201-46-242.ngrok.io")
+      .then((response) => response.json())
+      .then((data) => setQuiz(data));
+  }, []);
+
+  //   const quiz = [
+  //     {
+  //       answer: "pairs of people",
+  //       question: "What is a graph a collection of?",
+  //     },
+  //     {
+  //       answer: "Mirroring",
+  //       question: "What is the most common method for visualizing graphs?",
+  //     },
+  //     {
+  //       answer: "edges or arcs",
+  //       question: "What are the underlying objects being paired?",
+  //     },
+  //   ];
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <Container>
+      <Text
+        style={{
+          fontFamily: "A",
+          fontSize: 30,
+          textAlign: "center",
+          color: theme.grey,
+        }}
+      >
+        Quiz
+      </Text>
+      <View>
+        <CustomText
+          value="Questions"
+          size={25}
+          bold
+          style={{ marginTop: 20 }}
+        />
+        <FlatList
+          data={quiz}
+          renderItem={({ item, index }) => (
+            <FlipCard
+              style={{
+                marginVertical: 10,
+                flexDirection: "row",
+                alignItems: "center",
+                backgroundColor: theme.primaryColor,
+                padding: 20,
+                borderRadius: "10px",
+              }}
+            >
+              <View style={{ marginRight: 20 }}>
+                <CustomText
+                  value={item.question}
+                  color={theme.white}
+                  bold
+                ></CustomText>
+              </View>
+              <View style={{ marginRight: 20 }}>
+                <CustomText
+                  value={item.answer}
+                  color={theme.white}
+                  bold
+                ></CustomText>
+              </View>
+            </FlipCard>
+          )}
+        />
+      </View>
+    </Container>
+  );
+}
+const styles = StyleSheet.create({
+  historyItem: {
+    width: "100%",
+    backgroundColor: theme.primaryColor,
+    borderRadius: 8,
+    padding: 20,
+    height: 100,
+    marginVertical: 10,
+  },
+});
