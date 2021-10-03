ec47b696cac60662d67ae0060f5bca6f7d3d6036
Timothy Qin <timothy.t.qin@gmail.com>
2021-10-02 23:43:03 -0500

    renamed profile to home; reorganized features page

--- equiteach_app/screens/Features.js
+++ equiteach_app/screens/Features.js
@@ -1,19 +1,19 @@
 import React, { useState, useEffect, useRef, createRef } from "react";
 import { StyleSheet, View, Text } from "react-native";
-import { useNavigation } from '@react-navigation/native';
-import LottieView from 'lottie-react-native';
+import { useNavigation } from "@react-navigation/native";
+import LottieView from "lottie-react-native";
 import Container from "../components/Container";
 import CustomText from "../components/CustomText";
 import ProgressDots from "../components/ProgressDots";
 import { theme } from "../theme";
-
+import CustomButton from "../components/CustomButton";
 
 export default function Features() {
 	const navigation = useNavigation();
 	const [pageNumber, setPageNumber] = useState(0);
 	let animation = React.createRef();
 	useEffect(() => {
-		animation.current.play()
+		animation.current.play();
 	}, []);
 
 	const pages = [
@@ -21,56 +21,88 @@ export default function Features() {
 			title: "On-Demand Tutor Match",
 			description:
 				"Lorem ipsum dolor sit amet, consectetur  adipiscing elit, sed do eiusmod tempor  incididunt ut labore et dolore magna",
-			img: require('../assets/1.json')
+			img: require("../assets/1.json"),
 		},
 		{
 			title: "Micropayments",
 			description:
 				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna",
-			img: require('../assets/2.json')
+			img: require("../assets/2.json"),
 		},
 		{
 			title: "Feature 3",
 			description:
 				"Lorem ipsum dolor sit amet, consectetur  adipiscing elit, sed do eiusmod tempor  incididunt ut labore et dolore magna",
-			img: require('../assets/3.json')
+			img: require("../assets/3.json"),
 		},
 	];
 
-	
 	return (
-		<Container style={{ justifyContent: "center" }}>
-			<View style={{ alignItems: "center", paddingHorizontal: 40 }}>
-			<LottieView
-                ref={animation}
-                loop={true}
-                style={{
-                   width: 400,
-                   height: 350
-                }}
-                source={pages[pageNumber].img}
-           />
-				<CustomText
-					value={pages[pageNumber].title}
-					style={{ marginBottom: 10 }}
-					size={32}
-					center
-					bold
-				/>
-				<CustomText
-					value={pages[pageNumber].description}
-					color={theme.black}
-					center
-				/>
-				<ProgressDots
-					style={{ marginTop: 120 }}
-					index={pageNumber}
-					length={pages.length}
-					size={10}
-				/>
+		<Container style={{ flex: 1 }}>
+			<View style={{ flex: 1, justifyContent: "flex-end" }}>
+				<View
+					style={{
+						// flex: 1,
+						alignItems: "center",
+						paddingHorizontal: 40,
+						justifyContent: "flex-end",
+					}}
+				>
+					<LottieView
+						ref={animation}
+						loop={true}
+						style={{
+							width: 400,
+							height: 350,
+						}}
+						source={pages[pageNumber].img}
+					/>
+					<CustomText
+						value={pages[pageNumber].title}
+						style={{ marginBottom: 10 }}
+						size={32}
+						center
+						bold
+					/>
+					<CustomText
+						value={pages[pageNumber].description}
+						color={theme.black}
+						center
+					/>
+				</View>
+				<View
+					style={{
+						alignItems: "center",
+						justifyContent: "flex-end",
+					}}
+				>
+					<ProgressDots
+						index={pageNumber}
+						length={pages.length}
+						style={{ marginTop: 120 }}
+						size={10}
+					/>
+				</View>
+				<View
+					style={{
+						flex: 1,
+						alignItems: "center",
+						justifyContent: "flex-end",
+						marginBottom: 20,
+					}}
+				>
+					<CustomButton
+						text="Skip"
+						inverted
+						onPress={() => {
+							if (pageNumber == pages.length - 1) navigation.navigate("Login");
+							setPageNumber((pageNumber + 1) % pages.length);
+							animation.current.play();
+						}}
+						buttonStyle={{ borderRadius: 50, width: "50%" }}
+					/>
+				</View>
 			</View>
-			<Text onPress={()=>{setPageNumber((pageNumber + 1) % pages.length);animation.current.play();}} style={{textAlign:'center'}}>X</Text>
-			{pageNumber==2 && <Text onPress={()=>navigation.navigate('Login')}>X</Text>}
 		</Container>
 	);
 }
